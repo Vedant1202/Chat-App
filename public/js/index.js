1f1e4046e82053b69168de19447782cb87b3c457
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-11-03 17:58:12 +0530

    Added mesasge input and show functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,30 @@
 //  ==========================================================
 //  =================== SOCKET CONFIGURATION =================
 //  ==========================================================
- 
+
 var socket = io();
 
 // Connect handler
 socket.on('connect', function () {
   console.log("Connected to server!");
+});
 
-  var date = new Date();
-  var current_time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-  // socket.emit('createMessage', {                    // Client to Server
-  //   from: "Suarez",
-  //   text: "Good to see you, mate!",
-  //   createdAt: current_time
-  // });
+var date = new Date();
+socket.emit('createMessage', {                    // Client to Server
+  from: "Suarez",
+  text: "Good to see you, mate!",
+  createdAt: date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+}, function (data) {
+  console.log("Got the message! " + data);
 });
 
 socket.on('newMessage', function (message) {       // Server to Client
   console.log("New message!", message);
+
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#allMessages').append(li);
 });
 
 // Disconnect handler
@@ -31,3 +36,14 @@ socket.on('disconnect', function () {
 
 //  ***************** socket config ends *********************
 //  **********************************************************
+
+$('#msgForm').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name = message]').val()
+  }, function () {
+    console.log("Message");
+  });
+});
