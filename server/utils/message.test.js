939bf34851814508a76439b690d73440267d7820
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-11-06 00:12:39 +0530

    Added test for generate location

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,6 @@
 var expect            = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
+
 
 describe('generateMessage', function () {
   it('should generate correct message object', function () {
@@ -7,9 +8,27 @@ describe('generateMessage', function () {
         text    = 'I am rocketic!!';
         message = generateMessage(from, text);
 
-    expect(typeof(message.createdAt)).toBe('string'); // createdAt should be a number
+    expect(typeof(message.createdAt)).toBe('string'); // createdAt should be a string
     expect(message).toBeTruthy(); // assert 'from' and 'text' should return correctly and match
     expect(message.from).toEqual('Ivan');
     expect(message.text).toEqual('I am rocketic!!');
+  });
+});
+
+
+describe('generateLocationMessage', function () {
+  it('should generate correct location object', function () {
+    var fromUser    = 'Coutinho';
+        latitude    = 19;
+        longitude   = 72;
+        url         = generateLocationMessage(fromUser, latitude, longitude);
+
+
+     expect(typeof(message.createdAt)).toBe('string'); // createdAt should be a string
+     expect(url).toBeTruthy();   // Asser url to be truthy and check if it returns proper values
+     expect(url.from).toEqual('Coutinho');
+
+     var actualUrl = 'https://www.google.com/maps?q=' + latitude + ',' + longitude;
+     expect(url.url).toEqual(actualUrl);
   });
 });
